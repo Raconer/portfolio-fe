5ca87b7a35499784637174b38adcb0ee82349135
Dongho Kim <cha11enge@hanmail.net>
2024-05-06 19:54:43 +0900

    feat - Set Vuetify (#1)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore
+import vuetifyOptions from "./vuetify.options";
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: {enabled: true},
+
+    typescript: {
+        typeCheck: false
+    },
+    modules: ["vuetify-nuxt-module"],
+    vuetify: {
+        moduleOptions: {
+            /* module specific options */
+        },
+        vuetifyOptions
+    },
+    components: [
+        {
+            path: "~/components",
+            pathPrefix: false,
+        },
+    ],
 })
---
+++ vuetify.options.js
@@ -0,0 +1,29 @@
+export default ({
+    theme: {
+        defaultTheme: 'dark',
+        themes: {
+            dark: {
+                colors: {
+                    primary: '#33c',
+                    accent: '#33c',
+                    secondary: '#33c',
+                    info: '#33c',
+                    warning: '#33c',
+                    error: '#33c',
+                    success: '#33c',
+                },
+            },
+            light: {
+                colors: {
+                    primary: '#3fd',
+                    accent: '#3fd',
+                    secondary: '#3fd',
+                    info: '#3fd',
+                    warning: '#3fd',
+                    error: '#3fd',
+                    success: '#3fd',
+                }
+            },
+        }
+    }
+})
